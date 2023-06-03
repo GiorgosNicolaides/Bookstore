bb2b9031980725e1c7bf01ffb6a831547db8ed02
Giorgos <gnicolaides02@gmail.com>
2023-06-04 02:46:02 +0300

    commit

--- public/client.js
+++ public/client.js
@@ -31,44 +31,56 @@ nextBtn.addEventListener("click", () => {
 
 showSlide();
 document
-    .getElementById("register-form")
-    .addEventListener("submit", async function (e) {
-        e.preventDefault();
-        const author = document.getElementById("author").value;
-        const title = document.getElementById("title").value;
-        const genre = document.getElementById("genre").value;
-        const price = parseFloat(document.getElementById("price").value);
+  .getElementById("register-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const author = document.getElementById("author").value;
+    const title = document.getElementById("title").value;
+    const genre = document.getElementById("genre").value;
+    const price = parseFloat(document.getElementById("price").value);
 
-        // Validate the price input
-        if (isNaN(price)) {
-            alert("Please enter a valid price.");
-            return;
-        }
+    // Validate the price input
+    if (isNaN(price)) {
+      alert("Please enter a valid price.");
+      return;
+    }
 
-        const book = { author, title, genre, price };
-        const url = "http://localhost:3000/book/";
-        const result = await fetch(url, {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(book),
-        });
+    const book = { author, title, genre, price };
+    const url = "http://localhost:3000/book/";
 
-        const response = await result.json();
-        if (response.message === "Book added successfully") {
-            alert("Book added successfully");
-        } else {
-            alert("Failed to add book");
-        }
+    // Check if the book title already exists in the database
+    const checkUrl = `http://localhost:3000/book/${encodeURIComponent(title)}`;
+    const checkResult = await fetch(checkUrl);
+    const existingBooks = await checkResult.json();
 
-        // Clear the form
-        document.getElementById("author").value = "";
-        document.getElementById("title").value = "";
-        document.getElementById("genre").value = "";
-        document.getElementById("price").value = "";
+    if (existingBooks.length > 0) {
+      alert("Book already exists in the database.");
+      return;
+    }
+
+    // Add the book to the database
+    const result = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
     });
+
+    const response = await result.json();
+    if (result.status === 200) {
+      alert(response.message);
+    } else {
+      alert("Failed to add the book.");
+    }
+
+    // Clear the form
+    document.getElementById("author").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("price").value = "";
+  });
 
 document
   .getElementById("search-form")
@@ -86,7 +98,7 @@ document
       previousResults.innerHTML = ""; // Clear the results container
     }
 
-    // Remove the search bars text
+    // Remove the search bar's text
     document.getElementById("search").value = "";
     // Create a new div to display the search results
     const resultsDiv = document.createElement("div");
@@ -109,3 +121,9 @@ document
     const scrollContainer = document.getElementById("scroll-container");
     scrollContainer.appendChild(resultsDiv);
   });
+
+// Clear results when the clear button is clicked
+document.getElementById("clear-results").addEventListener("click", function () {
+  const resultsContainer = document.getElementById("scroll-container");
+  resultsContainer.innerHTML = ""; // Clear the results container
+});
