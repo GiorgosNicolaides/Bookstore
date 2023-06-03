382dbe595b31cdeb72a2933e6a685d78cf948c0b
Giorgos <gnicolaides02@gmail.com>
2023-06-03 17:57:53 +0300

    client and server

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get("/book/:title", async (req, res) => {
-  const q = `SELECT * FROM books WHERE title LIKE '%${req.params.title}%'`;
+  const title = req.params.title;
+  const title1 = title.substring(1); // Removes the first character
+  const q = `SELECT * FROM books WHERE title Like'%${title1}%'`;
+  console.log(q);
+
 
   try {
     const results = await query(q);
@@ -24,6 +28,11 @@ app.get("/book/:title", async (req, res) => {
   }
 });
 
+
+app.post('/book/',(req,res)=>{
+	console.log(req.body);
+	res.json(req.body);
+});
 
 
 function query(q) {
